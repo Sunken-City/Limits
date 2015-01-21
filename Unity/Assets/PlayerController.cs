323d2674e839900fd4c4d4c16d835fc8462b8bc2
picoriley <acloudy@mail.smu.edu>
2015-01-21 13:35:03 -0600

    Player now moves around in a very basic fashion :3

--- Unity/Assets/PlayerController.cs
+++ Unity/Assets/PlayerController.cs
@@ -1,15 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 1.0f;
+    // Use this for initialization
+    void Start()
+    {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        transform.position += transform.up * Input.GetAxis("Vertical") * speed * Time.deltaTime;
+    }
 }
