22a14b8c3711294bd26676fa436c8f2fe1fe6f10
picoriley <acloudy@mail.smu.edu>
2015-01-21 13:46:31 -0600

    Camera now follows the player

---
+++ Unity/Assets/CameraFollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    public Transform target;
+    public float smooth = 5.0f;
+
+    private Vector2 cameraFollow;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cameraFollow = Vector2.Lerp(transform.position, target.position, Time.deltaTime * smooth);
+        transform.position = new Vector3(cameraFollow.x, cameraFollow.y, -10f);
+    }
+}
